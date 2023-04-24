3601d650a7d066b478c407ce0e9c41fd9d867b96
Evgenia Smirnova <evgeniabright@yandex.ru>
2023-04-24 17:37:32 +0700

    dev: added celebrate npm

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
-const router = require('./routes');
+const { celebrate, Joi } = require('celebrate');
 
+const router = require('./routes');
 const { auth } = require('./middlewares/auth');
-const { NOT_FOUND } = require('./utils/errors');
 const { createUser, login } = require('./controllers/users');
 const { handleErrors } = require('./middlewares/handleErrors');
 const limiter = require('./utils/constants/limiter');
+const NotFoundError = require('./utils/errors/NotFoundError');
+const { regExp } = require('./utils/constants/regExp');
 
 const app = express(router);
 
@@ -18,17 +20,37 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 // функционал работы роутеров
-app.post('/signup', createUser);
-app.post('/signin', login);
+app.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  login,
+);
+
+app.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().pattern(regExp),
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  createUser,
+);
 app.use(auth);
 // Apply the rate limiting middleware to all requests
 app.use(limiter);
 app.use(router);
 
-app.use((req, res) => {
-  res.status(NOT_FOUND).send({
-    message: 'Такого адреса не существует',
-  });
+app.use((next) => {
+  next(new NotFoundError('Такого адреса не существует'));
 });
 
 app.use(handleErrors);
